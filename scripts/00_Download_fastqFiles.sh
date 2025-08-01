2d245ded4e3766153df682cd8252269588ea0436
Mojtaba Shaban Loushab <165825494+moji-2024@users.noreply.github.com>
2025-07-31 20:55:13 -0600

    Update 00_Download_fastqFiles.sh

--- scripts/00_Download_fastqFiles.sh
+++ scripts/00_Download_fastqFiles.sh
@@ -2,8 +2,21 @@
 
 mkdir -p data/raw/FastqFiles
 # Run wget to each url in Fast_urls.txt
-for url in $(cat data/raw/Fastq_urls.txt) ;do 
+for url in $(cat data/raw/Fastq_urls.txt) ;do
 echo "Processing $url..." ;
 wget -P ../data/raw/FastqFiles/ $url;
-done
+
+# Extract basename (filename only)
+file_name=$(basename "$url")
+# Extract SRR ID from filename
+BaseName=$(echo "$file_name" | grep -Eo "SRR[0-9]+")
+if [ "$BaseName" == "SRR13633855" ]; then
+  mv ../data/raw/FastqFiles/$file_name ../data/raw/FastqFiles/WT$file_name
+elif [ "$BaseName" == "SRR13633856" ]; then
+  mv ../data/raw/FastqFiles/$file_name ../data/raw/FastqFiles/WT$file_name
+else
+  mv ../data/raw/FastqFiles/$file_name ../data/raw/FastqFiles/KO$file_name
+fi
+
+done ;
 echo "all data is ready"
