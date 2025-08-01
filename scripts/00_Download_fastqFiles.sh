3ecb10b21e42c2bd7c2f3b08f5e2c4cd5aaba47b
Mojtaba Shaban Loushab <165825494+moji-2024@users.noreply.github.com>
2025-07-31 20:22:13 -0600

    Update 00_Download_fastqFiles.sh

--- scripts/00_Download_fastqFiles.sh
+++ scripts/00_Download_fastqFiles.sh
@@ -4,6 +4,6 @@ mkdir -p data/raw/FastqFiles
 # Run wget to each url in Fast_urls.txt
 for url in $(cat data/raw/Fastq_urls.txt) ;do 
 echo "Processing $url..." ;
-wget -P /data/raw/FastqFiles/ $url;
-doen
+wget -P ../data/raw/FastqFiles/ $url;
+done
 echo "all data is ready"
