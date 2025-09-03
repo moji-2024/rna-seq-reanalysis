988dd3dadaf4f6d414a4fb1aa451c7fb40eefb9a
Mojtaba Shaban Loushab <165825494+moji-2024@users.noreply.github.com>
2025-09-03 16:08:07 -0600

    Delete scripts/03_align_by_star.sh

--- scripts/03_align_by_star.sh
+++
@@ -1,7 +0,0 @@
-STAR --runThreadN 4 \
-     --genomeDir genome_index/ \
-     --readFilesIn data/raw/FastqFiles/WT_SRR13633855.fastq.gz \
-     --readFilesCommand zcat \
-     --outFileNamePrefix alignments/WT_SRR13633855_ \
-     --outSAMtype BAM SortedByCoordinate
-
